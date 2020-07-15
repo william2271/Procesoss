a78a0d33395e0b4dead3dcfc722970c8a7f52f02
unknown <William22711@gmail.com>
2020-07-15 15:13:57 -0400

    seguimos

--- Registro.php
+++ Registro.php
@@ -52,7 +52,7 @@ $crip4 = serialize($carrera);
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <h4 align= "Center"> Registro de estudiantes:<h4/>
+    <h4 align= "Center"> Procesos:<h4/>
     
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
@@ -64,99 +64,13 @@ $crip4 = serialize($carrera);
 </head>
 
 <body style="background-color:grey;">
-<div id="seccionRecargar"></div>
-<form action="#" method="POST">
-  
-<p>Nombre: <input type="text"  id="nombre" name="nombre" /></p>
-<p>Apellido:  <input type="text" id="apellido" name="apellido" /></p>
-<p>favorito:  <input type="text" id="favorito" name="favorito" />
-Estatus:<input type="checkbox" id="estatus"name="estatus">
-<p>Carreras: <select  id ="carrera" name="carrera">
-  
-<option selected disabled value="">Escoge</option>
-<option>Redes</option>
-        <option>Software</option>
-        <option>Multimedia</option>
-        <option>Mecatronica</option>
-        <option>Seguridad informatica</option>
-</select></p>
 
-<label>Seleccione una imagen:</label><br />
-        <input type="file" name="imagen" />
 
-<p><input type="submit"  class="btn btn-primary" name="guardar" onclick="return foo();" value="Aceptar/editar"/>
-<input type="reset"  class="btn btn-warning" value="reiniciar">
-</p>
-
-</form>
+<h4>En la tabla de abajo se visualiza la informacion de un csv:</h4>
 
 
   
-  <table class="table">
-    <thead>
-      <tr>
-        <th scope="col">Nombre</th>
-        <th scope="col">Apellido</th>
-        <th scope="col">Estado</th>
-        <th scope="col">Carrera</th>
-        <th scope="col">favorito</th>
-        <th scope="col">opciones.</th>
-        
-      
-      </tr>
-      </thead>
-        <tbody>
-        
-        
-        
-      <?php
-     
-
-    
-    foreach ($devolver as $actual)  { 
-    echo "<tr>";
-  
-    echo "<td>".$actual."</td>";
-    }
-    foreach($devolver2 as $actual2){  
-    echo "<td>".$actual2."</td>";
-   }
-   foreach($devolver3 as $actual3){   
-    echo "<td>".$actual3."</td>";
-    }
-    ?>
-   
-    <?php 
-     foreach($devolver4 as $actual4)  ?> 
-    <?php echo "<td>".$actual4."</td>";?>{  
-   <?php foreach($devolver5 as $actual5) ?>{
-    <?php echo "<td>".$actual5."</td>";?>
-   }
-  
-    <td>
-                           
-                            <button id="dos"  name="eliminar"  class="btn btn-danger btn-sm">
-                                <i class="material-icons md-18">eliminar</i>
-                            </button>
-                        </td>
-  <?php echo "</tr>"; ?>
-  }
-
-
-
  
-    
-  
-   <?php
-  
-  
-  ?>
- 
-  </tbody>
-</table>
- 
-
-</div>
 
 
 
@@ -170,24 +84,23 @@ Estatus:<input type="checkbox" id="estatus"name="estatus">
 
 
 <?php 
-//index.php 
+
  
-//ubicacion csv 
 define('CSV', '100Records.csv'); 
  
-//leer csv 
+
 $readCsv = array_map('str_getcsv', file(CSV)); 
 ?> 
-<html> 
- <table border="1"> 
+
+ <table class="table table-dark" border="1"> 
  <?php 
- //recorremos filas del csv 
+ 
  foreach ($readCsv as $itemCsv) { 
    echo '<tr>'; 
-   //recorremos celdas del csv 
+   
     foreach ($itemCsv as $elementoItemCSV) { 
      echo '<td>'; 
-     //mostramos la celda 
+     
      echo $elementoItemCSV; 
      echo '</td>'; 
     } 
@@ -195,13 +108,46 @@ $readCsv = array_map('str_getcsv', file(CSV));
    } 
   ?> 
  </table> 
-</html> 
 
 
+<a href="100Records.csv">Descargar csv.</a>
 
 
+<h4>Aqui abajo vemos la informacion de un json:</h4>
 
+<?php 
 
+ 
+define('JSONlocal', 'json.json');
+
+ 
+
+$readJSON = array_map('str_getcsv', file(JSONlocal)); 
+?> 
+
+ <table border="1"> 
+ <?php 
+ 
+ foreach ($readJSON as $itemCsv) { 
+   echo '<tr>'; 
+   
+    foreach ($itemCsv as $elementoItemCSV) { 
+     echo '<td>'; 
+     
+     echo $elementoItemCSV; 
+     echo '</td>'; 
+    } 
+    echo '</tr>'; 
+   } 
+  ?> 
+ </table> 
+ <a href="json.json">Descargar json.</a>
+<?php
+ $csv = serialize($readCsv); 
+  $json = serialize($readJSON);
+  
+
+?>
 <style>
     .md-18{
         font-size: 18px;
